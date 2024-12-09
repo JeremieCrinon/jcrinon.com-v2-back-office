951c868990626a870100993597f50f1ae4e37480
Jérémie Crinon <jeremie@jcrinon.com>
2024-12-09 17:10:35 +0100

    Translation the erorr pages.

--- src/pages/Errors/404.js
+++ src/pages/Errors/404.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useTranslation } from 'react-i18next';
+
 function NotFound() {
+
+  const { t } = useTranslation();
+
   return (
     <div className="container-fluid">
         <div className="text-center">
             <div className="error mx-auto" data-text="404">404</div>
-            <p className="lead text-gray-800 mb-5">Page Not Found</p>
-            <p className="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-            <Link to={"/"}>&larr; Back to home</Link>
+            <p className="lead text-gray-800 mb-5">{t('notFound.errorName')}</p>
+            <p className="text-gray-500 mb-0">{t('notFound.errorDescription')}</p>
+            <Link to={"/"}>&larr; {t('notFound.link')}</Link>
         </div>
 
     </div>
--- src/pages/Errors/500.js
+++ src/pages/Errors/500.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useTranslation } from 'react-i18next';
+
 function InternalServerError() {
+
+  const { t } = useTranslation();
+
   return (
     <div className="container-fluid">
         <div className="text-center">
             <div className="error mx-auto" data-text="500">500</div>
-            <p className="lead text-gray-800 mb-5">Internal server error</p>
-            <p className="text-gray-500 mb-0">Oh oh, that's not supposed to append, you can try again later, or contact me.... You can try to go back to the home using the link bellow.</p>
-            <Link to={"/"}>&larr; Back to home</Link>
+            <p className="lead text-gray-800 mb-5">{t('serverError.errorName')}</p>
+            <p className="text-gray-500 mb-0">{t('serverError.errorDescription')}</p>
+            <Link to={"/"}>&larr; {t('serverError.link')}</Link>
         </div>
 
     </div>
